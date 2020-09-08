2da3036221093e1a5c07bff258cf128dec6f477c
kirirotha <kirirotha@gmail.com>
2020-09-07 21:23:40 -0500

    added some clickability to board

--- frontend/source/index.js
+++ frontend/source/index.js
@@ -1,13 +1,37 @@
+TESTIMG="images/king_cat.jpg"
+
 document.addEventListener("DOMContentLoaded", () => {
-    
+    renderBoard()
+    //testRun() 
 })
 
 const renderBoard = () =>{
-
+    centerBox = document.querySelector(".center-box")
+    let gameTable = document.createElement("table")
+    gameTable.id = "game-board"
+    centerBox.appendChild(gameTable)
+    for(let i = 0; i < 10; i++){
+        let row = document.createElement('tr')
+        gameTable.appendChild(row)
+        for(let j = 0; j < 10; j++){
+            let cell = document.createElement('td')
+            cell.id = `${i}${j}`
+            row.appendChild(cell)
+            cellListener(cell)
+        }
+    }
 }
 
-const renderTitle = () =>{
+const cellListener = (cell) => {
+    cell.addEventListener('click', (e) =>{
+        console.log(e.target.id)
+        renderIconInCell(TESTIMG, e.target.id)
+    })
+}
 
+
+const renderTitle = () =>{
+    
 }
 
 const renderDugoutTop = () => {
@@ -16,4 +40,23 @@ const renderDugoutTop = () => {
 
 const renderDugoutBottom = () =>{
 
+}
+
+
+
+
+//--------------------------test stuff
+const testRun = () => {
+    renderIconInDugout(TESTIMG)
+}
+
+const renderIconInCell = (image, cellId) => {
+    let testImage = document.createElement("img")
+    testImage.src = image
+    testImage.height = 40
+    cell = document.getElementById(cellId)
+    cell.appendChild(testImage)
+    testImage.addEventListener('click', (e) => {
+        e.target.parentElement.style.backgroundColor = "green"
+    })
 }
